8191a130af10847d69749ab6bec8e7969eeba789
Igor <igorlfs@ufmg.br>
2024-12-24 14:59:43 -0300

    use const for SUBSCRIPTION_ID

--- lua/dap-view/events.lua
+++ lua/dap-view/events.lua
@@ -3,16 +3,17 @@ local state = require("dap-view.state")
 local breakpoints = require("dap-view.breakpoints.buf_content")
 local actions = require("dap-view.actions")
 local exceptions = require("dap-view.exceptions")
+local globals = require("dap-view.globals")
 
-dap.listeners.after.setBreakpoints["dap-view"] = function()
+dap.listeners.after.setBreakpoints[globals.SUBSCRIPTION_ID] = function()
     breakpoints.show()
 end
 
-dap.listeners.after.launch["dap-view"] = function()
+dap.listeners.after.launch[globals.SUBSCRIPTION_ID] = function()
     exceptions.update_exception_breakpoints_filters()
 end
 
-dap.listeners.after.initialize["dap-view"] = function(session, _)
+dap.listeners.after.initialize[globals.SUBSCRIPTION_ID] = function(session, _)
     state.exceptions_options = vim.iter(session.capabilities.exceptionBreakpointFilters)
         :map(function(filter)
             return { enabled = filter.default, exception_filter = filter }
@@ -20,10 +21,10 @@ dap.listeners.after.initialize["dap-view"] = function(session, _)
         :totable()
 end
 
-dap.listeners.before.event_terminated["dap-view"] = function()
+dap.listeners.before.event_terminated[globals.SUBSCRIPTION_ID] = function()
     actions.close()
 end
 
-dap.listeners.before.event_exited["dap-view"] = function()
+dap.listeners.before.event_exited[globals.SUBSCRIPTION_ID] = function()
     actions.close()
 end
--- lua/dap-view/globals.lua
+++ lua/dap-view/globals.lua
@@ -1,4 +1,5 @@
 return {
+    SUBSCRIPTION_ID = "dap-view",
     MAIN_BUF_NAME = "dap-view://main",
     NAMESPACE = vim.api.nvim_create_namespace("dap-view"),
 }

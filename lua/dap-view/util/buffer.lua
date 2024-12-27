7d1daf01c44eef33d31117865f34c1c3a918c5a7
Igor <igorlfs@ufmg.br>
2024-12-27 01:00:12 -0300

    refactor: cleanup dead code

--- lua/dap-view/util/buffer.lua
+++ lua/dap-view/util/buffer.lua
@@ -2,19 +2,6 @@ local M = {}
 
 local api = vim.api
 
----@param bufnr? integer
----@param winnr? integer
-M.close_buf_win = function(winnr, bufnr)
-    if winnr then
-        pcall(api.nvim_win_close, winnr, true)
-        winnr = nil
-    end
-    if bufnr then
-        pcall(api.nvim_buf_delete, bufnr, { force = true })
-        bufnr = nil
-    end
-end
-
 ---@param bufnr integer
 ---@param callback fun(): nil
 M.quit_buf_autocmd = function(bufnr, callback)

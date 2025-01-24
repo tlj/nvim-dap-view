8dcab410df73c3a256140fc1cadde772a5a6e3c3
tlj <tlj@tlj.no>
2025-01-24 09:31:53 +0100

    Fix for error in events after.initialize if session doesn't have exceptionBreakpointFilters

--- lua/dap-view/events.lua
+++ lua/dap-view/events.lua
@@ -48,11 +48,13 @@ dap.listeners.after.launch[SUBSCRIPTION_ID] = function()
 end
 
 dap.listeners.after.initialize[SUBSCRIPTION_ID] = function(session, _)
-    state.exceptions_options = vim.iter(session.capabilities.exceptionBreakpointFilters)
-        :map(function(filter)
-            return { enabled = filter.default, exception_filter = filter }
-        end)
-        :totable()
+    if session.capabilities.exceptionBreakpointFilters then
+        state.exceptions_options = vim.iter(session.capabilities.exceptionBreakpointFilters)
+            :map(function(filter)
+                return { enabled = filter.default, exception_filter = filter }
+            end)
+            :totable()
+    end
 end
 
 dap.listeners.after.event_terminated[SUBSCRIPTION_ID] = function()
